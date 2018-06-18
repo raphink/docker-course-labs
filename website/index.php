9810835123c697d5cd76233c7f6ed734df7c41c6
julien.waddle@camptocamp.com <julien.waddle@camptocamp.com>
2018-06-18 16:59:45 +0200

    init

---
+++ website/index.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title>C2C Shop</title>
+    </head>
+
+    <body>
+        <h1>Welcome to C2C</h1>
+        <ul>
+            <?php
+
+            $json = file_get_contents('http://product-service/');
+            $obj = json_decode($json);
+
+            $products = $obj->products;
+
+            foreach ($products as $product) {
+                echo "<li>$product</li>";
+            }
+
+            ?>
+        </ul>
+    </body>
+</html>
