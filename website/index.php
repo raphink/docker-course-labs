377809c52023c0fcb127068f72d0481def780e26
julien.waddle@camptocamp.com <julien.waddle@camptocamp.com>
2018-06-19 15:18:38 +0200

    without compose

--- website/index.php
+++ website/index.php
@@ -8,7 +8,7 @@
         <ul>
             <?php
 
-            $json = file_get_contents('http://product-service/');
+            $json = file_get_contents('http://pyapp/');
             $obj = json_decode($json);
 
             $products = $obj->products;
