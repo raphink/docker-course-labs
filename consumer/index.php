98f0445b6949922c47c104a0289eb56af269ed0e
Julien <julien.acroute@camptocamp.com>
2018-10-31 11:12:52 +0100

    Cleanup and add volume mount for dev

--- consumer/index.php
+++ consumer/index.php
@@ -1,22 +1,23 @@
 <html>
     <head>
-        <title>C2C Shop</title>
+        <title>C2C Projects</title>
     </head>
 
     <body>
         <h1>Welcome to C2C</h1>
         <ul>
             <?php
+            // Set producer URL
+            $producer = getenv('PRODUCER_SERVICE');
+            if(!$producer)
+              $producer = 'producer';
 
-            $json = file_get_contents('http://product-service/');
-            $obj = json_decode($json);
+            // Request products from producer
+            $response = json_decode(file_get_contents("http://$producer/"));
 
-            $products = $obj->products;
-
-            foreach ($products as $product) {
-                echo "<li>$product</li>";
-            }
-
+            // Display results
+            foreach ($response->products as $product)
+                echo "<li>$product</li>\n";
             ?>
         </ul>
     </body>
